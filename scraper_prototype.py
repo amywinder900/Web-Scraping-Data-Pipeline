dd7dddc65d00e925756948d627d178d5e302bacb
amywinder900 <amywinder98@gmail.com>
2022-01-20 16:58:54 +0000

    Implements cookies and login bypass.

--- scraper_prototype.py
+++ scraper_prototype.py
@@ -2,6 +2,8 @@
 #%%
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
+
 
 class scraper():
     def __init__(self, URL):
@@ -13,12 +15,22 @@ class scraper():
         time.sleep(1)
 
     def load_and_bypass(self) -> webdriver.Chrome:
+        email="johnsmith934812@gmail.com"
+        password="as8U6!s"
         self.navigate_to(self.URL)
         accept_cookies_button = self.driver.find_element_by_xpath("//*[@data-control-name='ga-cookie.consent.accept.v3']")
         accept_cookies_button.click()
-        time.sleep(30)
-
+        time.sleep(2)
+        email_input = self.driver.find_element_by_xpath("//*[@name='session_key']")
+        password_input = self.driver.find_element_by_xpath("//*[@name='session_password']")
+        email_input.send_keys(email)
+        password_input.send_keys(password)
+        password_input.send_keys(Keys.RETURN)
+        time.sleep(2)
+        # may be needed to bypass phone number pop up, wait and see
+        # skip_button =self.driver.find_element_by_xpath("//*[class='secondary-action']")
+        # skip_button.click()
 # %%
-linkedin = scraper("https://www.linkedin.com/searchd")
+linkedin = scraper("https://www.linkedin.com")
 linkedin.load_and_bypass()
 # %%
