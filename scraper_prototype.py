9ad17668aa1cb22d130c166339ed33cdedb11672
amywinder900 <amywinder98@gmail.com>
2022-02-07 19:45:12 +0000

    Refactors the method to collect data from a product page

--- scraper_prototype.py
+++ scraper_prototype.py
@@ -1,5 +1,6 @@
 
 # %%
+from gc import collect
 import os
 import json
 import requests
@@ -7,6 +8,7 @@ import time
 import uuid
 from selenium import webdriver
 from pathlib import Path
+from selenium.webdriver.remote.webelement import WebElement
 # %%
 
 
@@ -14,8 +16,8 @@ class Scraper:
     """
     This class is used to scrape product data from gear4music.com
 
-    Attributes: 
-        webpage_url (str): The URL of the product type to be scraped.  
+    Attributes:
+        webpage_url (str): The URL of the product type to be scraped.
 
     """
 
@@ -36,12 +38,13 @@ class Scraper:
         accept_cookies_button = self.driver.find_element_by_xpath(
             "//button[@id='banner-cookie-consent-allow-all']")
         accept_cookies_button.click()
+        return None
 
     def __retrieve_links_from_current_page(self) -> list:
         """
         A private method which retrieves links from the driver's current page.
 
-        Returns: 
+        Returns:
             page_list_of_product_urls(list): The list of the urls on the current page of the driver.
         """
         time.sleep(2)
@@ -55,7 +58,7 @@ class Scraper:
         """
         Retrieves all product links for this driver.
 
-        Returns: 
+        Returns:
             list_of_product_urls(list): The list of urls for products of this type.
         """
         self.__accept_cookies()
@@ -69,9 +72,10 @@ class Scraper:
             self.driver.get(url)
             new_product_urls = self.__retrieve_links_from_current_page()
             list_of_product_urls.extend(new_product_urls)
-            #Removes duplicates from the list of urls
-            list_of_product_urls = [i for n, i in enumerate(list_of_product_urls)if i not in list_of_product_urls[:n]]
-            #Stops the loop if no new product urls have been collected
+            # Removes duplicates from the list of urls
+            list_of_product_urls = [i for n, i in enumerate(
+                list_of_product_urls)if i not in list_of_product_urls[:n]]
+            # Stops the loop if no new product urls have been collected
             new_total_urls_collected = len(list_of_product_urls)
             if new_total_urls_collected == unique_urls_collected:
                 break
@@ -83,35 +87,45 @@ class Scraper:
 
         return list_of_product_urls
 
-    def collect_data_for_product(self, product_url: str)->dict:
+    def check_stock(self) -> str:
         """
-        Collects the data and image links for a given product.
+        This method checks the stock of a product. 
 
-        Args: 
-            product_url(str): The url of the product to retrieve data from.
-        
-        Returns: 
-            data(dict): The dictionary of data for the product.
+        Returns:
+            stock(str): The stock number of the product on the webdriver's current page.
         """
-        print("Collecting data from", product_url)
-        time.sleep(1)
-        self.driver.get(product_url)
-        product_ref = self.driver.find_element_by_xpath(
-            "//p[@class='prd-ref']").text.split(":")[1].strip()
-        product_uuid = uuid.uuid4()
-        product_name = self.driver.find_element_by_xpath(
-            "//h1[@itemprop='name']").text
-        price = self.driver.find_element_by_xpath(
-            "//span[@class='c-val']").text
         stock_container = self.driver.find_elements_by_xpath(
             "//div[@class='tooltip-container info-row-stock info-row-item']/div")[0]
-        if stock_container.get_attribute("class")=="tooltip-source info-row-stock-msg instock in-stock":
+        if stock_container.get_attribute("class") == "tooltip-source info-row-stock-msg instock in-stock":
             stock = stock_container.text.split()[0]
         else:
             stock = stock_container.text
-        description = self.driver.find_element_by_xpath(
-            "//div[@class='slide']").text.split(":", 1)[1]
-        # retrieve the images
+        return stock
+
+    def check_sale(self) -> bool:
+        """
+        This method checks whether a product is on sale. 
+
+        Returns:
+            sale(bool): True if the product on the webdriver's current page is on sale.
+        """
+        special_messages = self.driver.find_elements_by_xpath(
+            "//div[@class='info-row-special-msg info-row-item']"
+        )
+        sale = False
+        for message in special_messages:
+            if message.text == "SALE":
+                sale = True
+        return sale
+
+    def collect_image_links(self) -> list:
+        """
+        This method retrieves the links for the images of a product. 
+
+
+        Returns:
+            images(list): The links to the images of the product on the webdriver's current page.
+        """
         main_image = self.driver.find_element_by_xpath(
             "//img[@class='main-image']"
         ).get_attribute("src")
@@ -121,34 +135,40 @@ class Scraper:
         images = [image.get_attribute(
             "href") for image in other_images_container]
         images.insert(0, main_image)
-        # checks if it's flagged as a sale
-        special_messages = self.driver.find_elements_by_xpath(
-            "//div[@class='info-row-special-msg info-row-item']"
-        )
-        sale = False
-        for message in special_messages:
-            if message.text == "SALE":
-                sale = True
-        # Creates dictionary of the data.
-        data = {"product_uuid": str(product_uuid),
-                "product_ref": product_ref,
-                "product_name": product_name,
-                "price": price,
-                "stock": stock,
-                "description": description,
-                "images": images,
-                "sale": sale}
+        return images
+
+    def collect_data_for_product(self, product_url: str) -> dict:
+        """
+        Collects the data and image links for a given product.
+
+        Args: 
+            product_url(str): The url of the product to retrieve data from.
+
+        Returns: 
+            data(dict): The dictionary of data for the product.
+        """
+        print("Collecting data from", product_url)
+        time.sleep(1)
+        self.driver.get(product_url)
+        data = {"product_uuid": str(uuid.uuid4()),
+                "product_ref": self.driver.find_element_by_xpath("//p[@class='prd-ref']").text.split(":")[1].strip(),
+                "product_name": self.driver.find_element_by_xpath("//h1[@itemprop='name']").text,
+                "price": self.driver.find_element_by_xpath("//span[@class='c-val']").text,
+                "stock": self.check_stock(),
+                "description": self.driver.find_element_by_xpath("//div[@class='slide']").text.split(":", 1)[1],
+                "images": self.collect_image_links(),
+                "sale": self.check_sale()}
         return data
 
     @staticmethod
-    def __create_directories(product_ref:str):
+    def __create_directories(product_ref: str):
         """
         Creates directories required to scrape data for a product.
 
 
         Args:
             product_ref(str): The reference number of the product.
-        
+
         """
         current_directory = os.getcwd()
         data_directory = os.path.join(current_directory, "raw_data")
@@ -169,6 +189,7 @@ class Scraper:
         file_location = os.path.join(product_directory, "data.json")
         with open(file_location, "w") as f:
             json.dump(data, f)
+        return None
 
     @staticmethod
     def __download_image(file_location: str, image_url: str):
@@ -182,6 +203,8 @@ class Scraper:
         image_data = requests.get(image_url).content
         with open(file_location, "wb") as f:
             f.write(image_data)
+
+        return None
 
     @staticmethod
     def save_images_to_directory(data: dict, image_directory):
@@ -211,19 +234,23 @@ class Scraper:
             Scraper.save_data_to_file(data, product_directory)
             Scraper.save_images_to_directory(data, image_directory)
 
+        return None
+
     def close_scraper(self):
         """
         Closes the windows associated with the scraper. 
         """
         self.driver.quit()
 
+        return None
+
 
 # %%
 if __name__ == "__main__":
-    gear4music = Scraper("https://www.gear4music.com/dj-equipment/mobile-dj/microphones?_gl=1*1wxixgz*_ga*MjE1MDU3NzY1LjE2NDM4MTQ0NzE.*_up*MQ..")
+    gear4music = Scraper(
+        "https://www.gear4music.com/dj-equipment/mobile-dj/microphones?_gl=1*1wxixgz*_ga*MjE1MDU3NzY1LjE2NDM4MTQ0NzE.*_up*MQ..")
     gear4music.retrieve_product_links()
     gear4music.collect_data_and_store()
     gear4music.close_scraper
 
-# %%
 # %%
